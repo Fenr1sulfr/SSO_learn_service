28726a129c5b31c002f32c82ac4cf5c6e67fb56d
gittesting <120082611+Fenr1sulfr@users.noreply.github.com>
2024-12-16 23:45:03 +0500

    grpc handlers for starting app

---
+++ sso/internal/grpc/auth/auth.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"context"
+	g1 "sso/protos/gen/go/sso"
+
+	"google.golang.org/grpc"
+)
+
+type serverAPI struct {
+	g1.UnimplementedAuthServer
+}
+
+func Register(gRPC *grpc.Server) {
+	g1.RegisterAuthServer(gRPC, &serverAPI{})
+}
+
+func (s *serverAPI) Login(ctx context.Context, req *g1.LoginRequest) (*g1.LoginResponse, error) {
+	panic("Implement me")
+}
+func (s *serverAPI) Register(ctx context.Context, req *g1.RegisterRequest) (*g1.RegisterResponse, error) {
+	panic("implement me")
+}
+
+func (s *serverAPI) IsAdmin(ctx context.Context, req *g1.IsAdminRequest) (*g1.IsAdminResponse, error) {
+	panic("implement me")
+}
